4fe52335b63457d01189aa6a54fc2d3315ea6c2f
chrisbacon <bacon.chris@gmail.com>
2017-02-21 15:55:39 +0000

    added user and favourite shows relationships

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 Show.delete_all();
 
-Show.create({
+gbbo = Show.create({
   title: "The Great British Bake Off",
   series: 7,
   description: "Master bakers Mary & Paul and the incomparable presenting duo of Mel & Sue are back for another interesting series about baking cakes.",
@@ -8,10 +8,17 @@ Show.create({
   programmeID: "b013pqnm" 
 });
 
-Show.create({
+bojack = Show.create({
   title: "Bojack Horseman",
   series: 3,
   description: "Meet the most beloved sitcom horse of the '90s - 20 years later. BoJack Horseman was the star of the hit TV show Horsin' Around, but today he's washed up, living in Hollywood, complaining about everything, and wearing colorful sweaters.",
   image: "placeholder.jpg",
   programmeID: "b232mfew" 
 });
+
+chris = User.create({
+  name: "Chris"
+  });
+
+chris.favourites << bojack
+
